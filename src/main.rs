5181760907239894b599405056c389dcaad027cb
jenkin <jenkinpan028@gmail.com>
2024-06-20 13:56:09 +0800

    remove unused import

--- src/main.rs
+++ src/main.rs
@@ -3,9 +3,9 @@
 // modules ...
 use ::iced::theme::Theme;
 use iced::alignment::{Horizontal, Vertical};
-use iced::widget::{button, container, text, Button, Text, TextInput};
+use iced::widget::{button, container, text, Button, Column, Container, Row, TextInput};
 use iced::{
-    event, Background, Border, Element, Length, Padding, Sandbox, Settings, Shadow, Vector,
+    Alignment, Background, Border, Element, Length, Padding, Sandbox, Settings, Shadow, Vector,
 };
 
 // main entry point ...
@@ -73,22 +73,64 @@ impl Sandbox for RustUI {
     // define the update method ...
     fn update(&mut self, message: Message) {
         match message {
-            Message::ToggleTheme => {}
-            Message::LoginFieldChanged(email, password) => {}
+            Message::ToggleTheme => {
+                self.theme = if self.theme == Theme::Light {
+                    Theme::Dark
+                } else {
+                    Theme::Light
+                }
+            }
+            Message::LoginFieldChanged(email, password) => {
+                self.login_field.email = email;
+                self.login_field.password = password;
+            }
             Message::LoginSubmit => {}
-            Message::Router(route) => {}
+            Message::Router(route) => {
+                if route == "login" {
+                    self.page = Page::Login;
+                } else if route == "register" {
+                    self.page = Page::Register;
+                }
+            }
         }
     }
 
     // define the view method => this is where the UI goes ...
     fn view(&self) -> Element<Message> {
-        let btn = submit_btn("Test Button", Message::ToggleTheme);
+        let content = match self.page {
+            Page::Login => log_in_page(&self.login_field),
+            Page::Register => register_page(),
+        };
 
-        container(btn)
+        let wrapper = Column::new()
+            .spacing(50)
+            .width(Length::Fill)
+            .align_items(Alignment::Center)
+            .push(content)
+            .push(match self.page {
+                Page::Login => page_footer(
+                    button("Page Two")
+                        .on_press(Message::Router("register".to_string()))
+                        .style(iced::theme::Button::Custom(Box::new(
+                            ButtonStyle::ThemeButton,
+                        ))),
+                ),
+                Page::Register => page_footer(
+                    button("Main Page - Login")
+                        .on_press(Message::Router("login".to_string()))
+                        .style(iced::theme::Button::Custom(Box::new(
+                            ButtonStyle::ThemeButton,
+                        ))),
+                ),
+            });
+
+        container(wrapper)
             .width(Length::Fill)
             .height(Length::Fill)
+            .padding(Padding::from(20))
             .center_x()
             .center_y()
+            .style(iced::theme::Container::Custom(Box::new(ContainerStyle)))
             .into()
     }
 }
@@ -96,10 +138,49 @@ impl Sandbox for RustUI {
 // setup the different UI components
 
 // page footer ...
+fn page_footer(btn: Button<Message>) -> Container<Message> {
+    let footer = Row::new()
+        .push(button("Toggle Theme").on_press(Message::ToggleTheme).style(
+            iced::theme::Button::Custom(Box::new(ButtonStyle::ThemeButton)),
+        ))
+        .push(btn)
+        .align_items(Alignment::Center)
+        .spacing(10);
+    container(footer).center_x().center_y()
+}
 
 // login page / first page ...
+fn log_in_page(login_field: &LoginField) -> Container<Message> {
+    let column = Column::new()
+        .push(text("Graphical User Interface - Iced"))
+        .push(
+            input_field("Email Address ...", &login_field.email)
+                .on_input(|email| Message::LoginFieldChanged(email, login_field.password.clone())),
+        )
+        .push(
+            input_field("Password ...", &login_field.password).on_input(|password| {
+                Message::LoginFieldChanged(login_field.email.clone(), password)
+            }),
+        )
+        .push(submit_btn("Login", Message::LoginSubmit))
+        .padding(Padding::from([50, 20]))
+        .align_items(Alignment::Center)
+        .spacing(40);
+    container(column)
+        .padding(Padding::from(20))
+        .style(iced::theme::Container::Custom(Box::new(ContainerStyle)))
+}
 
 // register page / second page ...
+fn register_page() -> Container<'static, Message> {
+    let column = Column::new().push(text("Page two").size(64));
+
+    container(column)
+        .width(Length::Fill)
+        .height(Length::Fill)
+        .center_x()
+        .center_y()
+}
 
 // input field ...
 fn input_field(_placeholder: &str, _value: &str) -> TextInput<'static, Message> {
